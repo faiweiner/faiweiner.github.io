12f42d498975ad00257fc2339738dfe49247af01
Fai Weiner <fai.weiner@gmail.com>
2014-09-03 18:09:53 +1000

    main.js tweak

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-jQuery(function($) {
-	var options = {
-		$menu: false,
-		menuSelector: 'a',
-		panelSelector: 'section',
-		namespace: '.panelSnap',
-		onSnapStart: function(){},
-		onSnapFinish: function(){},
-		onActivate: function(){},
-		directionThreshold: 50,
-		slideSpeed: 200,
-		keyboardNavigation: {
-			enabled: false,
-			nextPanelKey: 40,
-			previousPanelKey: 38,
-			wrapAround: true
-		}
-	};
-
-	$('.panel_container').panelSnap(options);
+$(document).ready(function () {
+	$('.body').animate({ scrollTop: $('.panel_container').offset().top }, 3000);
 });
