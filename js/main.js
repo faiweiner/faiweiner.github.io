2e24752cbe60d24ec9ef1b4b0223f2d72ccc3601
Fai Weiner <fai.weiner@gmail.com>
2014-09-03 17:04:01 +1000

    snapscroll implementation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+jQuery(function($) {
+	var options = {
+		$menu: false,
+		menuSelector: 'a',
+		panelSelector: 'section',
+		namespace: '.panelSnap',
+		onSnapStart: function(){},
+		onSnapFinish: function(){},
+		onActivate: function(){},
+		directionThreshold: 50,
+		slideSpeed: 200,
+		keyboardNavigation: {
+			enabled: false,
+			nextPanelKey: 40,
+			previousPanelKey: 38,
+			wrapAround: true
+		}
+	};
 
+	$('.panel_container').panelSnap(options);
+});
