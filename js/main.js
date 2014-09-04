95b43499ea22110ccbe4346faa9c241610ae722a
Fai Weiner <fai.weiner@gmail.com>
2014-09-04 13:02:48 +1000

    added bootstrap

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
-$(document).ready(function () {
-	$('.body').animate({ scrollTop: $('.panel_container').offset().top }, 3000);
+$(document).ready(function() {
+	function init() {
+		window.addEventListener('scroll', function (e) {
+			var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+				shrinkOn = 300,
+				header = document.querySelector("header");
+			if (distanceY > shrinkOn) {
+				classie.add(header,"smaller");
+				console.log("i got smaller");
+			} else {
+				if (classie.has(header,"smaller")) {
+						classie.remove(header,"smaller");
+						console.log("i got bigger");
+				}
+			}
+		});
+	}
+
+	window.onload = init();
+
+	$('.avatar').hover(function () {
+		$( this ).css('padding', '5px' );
+			}, function() {
+		$( this ).css('padding', '2px' );
+		}
+	);
 });
