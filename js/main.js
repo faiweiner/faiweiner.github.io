9bc956a7c0fb808e4d39d157912bfc342b388e03
Fai Weiner <fai.weiner@gmail.com>
2014-09-08 14:01:38 +1000

    overall look and feel finished

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
+	// grab an element
+	var myElement = document.querySelector('header');
+	var someElement = document.querySelector('.container');
+
 	function init() {
 		window.addEventListener('scroll', function (e) {
-			var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-				shrinkOn = 300,
-				header = document.querySelector("header");
+			var distanceY = window.pageYOffset || document.documentElement.scrollTop;
+			var shrinkOn = 200;
 			if (distanceY > shrinkOn) {
-				classie.add(header,"smaller");
+				classie.add(myElement,"smaller");
 				console.log("i got smaller");
 			} else {
-				if (classie.has(header,"smaller")) {
-						classie.remove(header,"smaller");
+				if (classie.has(myElement,"smaller")) {
+						classie.remove(myElement,"smaller");
 						console.log("i got bigger");
 				}
 			}
@@ -17,136 +20,70 @@ $(document).ready(function() {
 	}
 
 	window.onload = init();
+
+	// construct an instance of Headroom, passing the element
+	var headroom  = new Headroom(myElement);
+	// initialise
+	headroom.init({
+		// vertical offset in px before element is first unpinned
+		offset : 400,
+		// scroll tolerance in px before state changes
+		tolerance : 600,
+		// or you can specify tolerance individually for up/down scroll
+		tolerance : {
+				up : 5,
+				down : 0
+		},
+		// css classes to apply
+		classes : {
+				// when element is initialised
+				initial : 'headroom',
+				// when scrolling up
+				pinned : 'headroom--pinned',
+				// when scrolling down
+				unpinned : 'headroom--unpinned',
+				// when above offset
+				top : 'headroom--top',
+				// when below offset
+				notTop : 'headroom--not-top'
+		},
+		// element to listen to scroll events on, defaults to `window`
+		scroller : someElement,
+		// callback when pinned, `this` is headroom object
+		onPin : function() {
+		},
+		// callback when unpinned, `this` is headroom object
+		onUnpin : function() {},
+		// callback when above offset, `this` is headroom object
+		onTop : function() {},
+		// callback when below offset, `this` is headroom object
+		onNotTop : function() {},
+	});
+	getViewport = function () {
+	  var m = document.compatMode == 'CSS1Compat';
+	  return {
+	    l : window.pageXOffset || (m ? document.documentElement.scrollLeft : document.body.scrollLeft),
+	    t : window.pageYOffset || (m ? document.documentElement.scrollTop : document.body.scrollTop),
+	    w : window.innerWidth || (m ? document.documentElement.clientWidth : document.body.clientWidth),
+	    h : window.innerHeight || (m ? document.documentElement.clientHeight : document.body.clientHeight)
+	  };
+	};
+
+
+	getViewport2 = function() {
+	    var $w = $(window);
+	    return {
+	        l: $w.scrollLeft(),
+	        t: $w.scrollTop(),
+	        w: $w.width(),
+	        h: $w.height() 
+	    }
+	}
+	        
+	console.log(getViewport());
+	    
+	console.log(getViewport2());
 });
 
 
 
-/**
- * cbpHorizontalSlideOutMenu.js v1.0.0
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2013, Codrops
- * http://www.codrops.com
- */
-;( function( window ) {
-     
-    'use strict';
- 
-    var document = window.document;
- 
-    function extend( a, b ) {
-        for( var key in b ) { 
-            if( b.hasOwnProperty( key ) ) {
-                a[key] = b[key];
-            }
-        }
-        return a;
-    }
- 
-    function cbpHorizontalSlideOutMenu( el, options ) { 
-        this.el = el;
-        this.options = extend( this.defaults, options );
-        this._init();
-    }
- 
-    cbpHorizontalSlideOutMenu.prototype = {
-        defaults : {},
-        _init : function() {
-            this.current = -1;
-            this.touch = Modernizr.touch;
-            this.menu = this.el.querySelector( '.cbp-hsmenu' );
-            this.menuItems = this.el.querySelectorAll( '.cbp-hsmenu > li' );
-            this.menuBg = document.createElement( 'div' );
-            this.menuBg.className = 'cbp-hsmenubg';
-            this.el.appendChild( this.menuBg );
-            this._initEvents();
-        },
-        _openMenu : function( el, ev ) {
-             
-            var self = this,
-                item = el.parentNode,
-                items = Array.prototype.slice.call( this.menuItems ),
-                submenu = item.querySelector( '.cbp-hssubmenu' ),
-                closeCurrent = function( current ) {
-                    var current = current || self.menuItems[ self.current ];
-                    current.className = '';
-                    current.setAttribute( 'data-open', '' );    
-                },
-                closePanel = function() {
-                    self.current = -1;
-                    self.menuBg.style.height = '0px';
-                };
- 
-            if( submenu ) {
- 
-                ev.preventDefault();
- 
-                if( item.getAttribute( 'data-open' ) === 'open' ) {
-                    closeCurrent( item );
-                    closePanel();
-                }
-                else {
-                    item.setAttribute( 'data-open', 'open' );
-                    if( self.current !== -1 ) {
-                        closeCurrent();
-                    }
-                    self.current = items.indexOf( item );
-                    item.className = 'cbp-hsitem-open';
-                    self.menuBg.style.height = submenu.offsetHeight + 'px';
-                }
-            }
-            else {
-                if( self.current !== -1 ) {
-                    closeCurrent();
-                    closePanel();
-                }
-            }
- 
-        },
-        _initEvents : function() {
-             
-            var self = this;
- 
-            Array.prototype.slice.call( this.menuItems ).forEach( function( el, i ) {
-                var trigger = el.querySelector( 'a' );
-                if( self.touch ) {
-                    trigger.addEventListener( 'touchstart', function( ev ) { self._openMenu( this, ev ); } );
-                }
-                else {
-                    trigger.addEventListener( 'click', function( ev ) { self._openMenu( this, ev ); } );    
-                }
-            } );
-             
-            window.addEventListener( 'resize', function( ev ) { self._resizeHandler(); } );
- 
-        },
-        // taken from https://github.com/desandro/vanilla-masonry/blob/master/masonry.js by David DeSandro
-        // original debounce by John Hann
-        // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
-        _resizeHandler : function() {
-            var self = this;
-            function delayed() {
-                self._resize();
-                self._resizeTimeout = null;
-            }
- 
-            if ( this._resizeTimeout ) {
-                clearTimeout( this._resizeTimeout );
-            }
- 
-            this._resizeTimeout = setTimeout( delayed, 50 );
-        },
-        _resize : function() {
-            if( this.current !== -1 ) {
-                this.menuBg.style.height = this.menuItems[ this.current ].querySelector( '.cbp-hssubmenu' ).offsetHeight + 'px';
-            }
-        }
-    }
- 
-    // add to global namespace
-    window.cbpHorizontalSlideOutMenu = cbpHorizontalSlideOutMenu;
- 
-} )( window );
